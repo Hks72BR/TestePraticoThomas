c9b1d1458808e2379bc784bc3ce9367ff6019251
Thomasnsa <thomasnsa935@gmail.com>
2024-10-16 15:29:00 -0300

    finalizado

--- RelatorioPython/Relatorio.py
+++ RelatorioPython/Relatorio.py
@@ -1,5 +1,6 @@
 import requests as re
 import json
+from pathlib import Path
 
 response  = re.get('http://localhost:5221/Tarefa')
 lista = response.text
@@ -8,14 +9,19 @@ lista_final = json.loads(lista)
 
 TarefaFinalizada = 0
 TarefaPendente = 0
+TotalTarefas = 0
 
 for chave in lista_final:
     if chave['finalizadoTarefa'] == True: TarefaFinalizada +=1
     if chave['finalizadoTarefa'] == False: TarefaPendente  +=1
 
+TotalTarefas = TarefaFinalizada + TarefaPendente
+
+TextoTotalTarefas = str(TotalTarefas) + " Tarefa(s) na lista."
 TextoTarefasFinalizas = str(TarefaFinalizada) + " tarefa(s) finalizada(s)."
 TextoTarefasPendentes = str(TarefaPendente) + " tarefa(s) pendente(s)."
 
-with open('Relatório de tarefas.txt', 'w') as arquivo:
-    arquivo.write(TextoTarefasFinalizas + "\n" + TextoTarefasPendentes)
-        
+desktop = Path.home() / "Desktop"
+
+with open(desktop /'Relatório de tarefas.txt', 'w') as arquivo:
+    arquivo.write(TextoTotalTarefas + "\n" + TextoTarefasFinalizas + "\n" + TextoTarefasPendentes)
